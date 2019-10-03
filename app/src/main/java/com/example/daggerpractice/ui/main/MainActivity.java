65f4369d896fd1a3b0ab00353cef4ad820a084b4
venkatesh <venkatesh.n@techtreeit.com>
2019-10-03 13:02:51 +0530

    Jetpack nav drawer.

--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -4,28 +4,42 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.daggerpractice.BaseActivity;
 import com.example.daggerpractice.R;
-import com.example.daggerpractice.ui.main.posts.PostsFragment;
-import com.example.daggerpractice.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testFragment();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        init();
     }
 
-    private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostsFragment())
-                .commit();
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -40,7 +54,48 @@ public class MainActivity extends BaseActivity {
             case R.id.logout:
                 sessionManager.logout();
                 return true;
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else {
+                    return false;
+                }
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true)
+                        .build();
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(
+                        R.id.profile_screen,
+                        null,
+                        navOptions
+                );
+                break;
+            case R.id.nav_posts:
+                if (isValidDestination(R.id.posts_screen))
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.posts_screen);
+                break;
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private boolean isValidDestination(int destination) {
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment).getCurrentDestination().getId();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout);
     }
 }
