2f1110ded2ed92ba3aa7531d4f389d5ba52029e7
venkatesh <venkatesh.n@techtreeit.com>
2019-10-03 14:36:07 +0530

    creating custom scopes.

--- app/src/main/java/com/example/daggerpractice/dependency_injection/ActivityBuildersModule.java
+++ app/src/main/java/com/example/daggerpractice/dependency_injection/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.example.daggerpractice.dependency_injection;
 
 import com.example.daggerpractice.dependency_injection.auth.AuthModule;
+import com.example.daggerpractice.dependency_injection.auth.AuthScope;
 import com.example.daggerpractice.dependency_injection.auth.AuthViewModelsModule;
 import com.example.daggerpractice.dependency_injection.main.MainFragmentBuildersModule;
 import com.example.daggerpractice.dependency_injection.main.MainModule;
+import com.example.daggerpractice.dependency_injection.main.MainScope;
 import com.example.daggerpractice.dependency_injection.main.MainViewModelsModule;
 import com.example.daggerpractice.ui.auth.AuthActivity;
 import com.example.daggerpractice.ui.main.MainActivity;
@@ -14,11 +16,13 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelsModule.class, AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainViewModelsModule.class, MainFragmentBuildersModule.class, MainModule.class}
     )
--- app/src/main/java/com/example/daggerpractice/dependency_injection/auth/AuthModule.java
+++ app/src/main/java/com/example/daggerpractice/dependency_injection/auth/AuthModule.java
@@ -8,6 +8,7 @@ import retrofit2.Retrofit;
 
 @Module
 public class AuthModule {
+    @AuthScope
     @Provides
     static AuthApi provideAuthApi(Retrofit retrofit) {
         return retrofit.create(AuthApi.class);
